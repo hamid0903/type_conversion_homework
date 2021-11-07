f5e7c264adc5d81e69d5a80f4fc31af2d54d439b
codeschooluz <89060347+codeschooluz@users.noreply.github.com>
2021-11-07 18:05:23 +0900

    template

---
+++ type01.py
@@ -0,0 +1,9 @@
+#Create a variable called 'var' and assign with data type integer to it.
+ 
+#Convert 'var' to float data type and assign to another variable called 'var_float'
+ 
+#Convert 'var' to string data type and assign to another variable called 'var_str'
+ 
+#Print the value of 'var_float'
+ 
+#Print the value of 'var_str'
---
+++ type02.py
@@ -0,0 +1,7 @@
+#Create a variable called 'var_int' and assign it an integer value.
+ 
+#Create a variable called 'var_float' and assign it a float value.
+ 
+#Create a variable called 'answer' and assign it the sum of var_int and var_float.
+ 
+#Print the type of answer.

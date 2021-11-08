2ada3e83664ce18e599b19f08397b176dc06db6c
Khamid Kamalov <hamid_kamalov@mail.ru>
2021-11-08 21:00:18 +0500

    float+int

--- type02.py
+++ type02.py
@@ -5,3 +5,7 @@
 #Create a variable called 'answer' and assign it the sum of var_int and var_float.
  
 #Print the type of answer.
+var_int=10
+var_float=15.5
+answer=var_int+var_float
+print(answer)
